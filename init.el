1430e6d40473108295ccbd13ef7fe80fcaa1fb59
theboswell <e.boswell2012@gmail.com>
2014-06-07 16:55:23 -0500

    add init

--- init.el
+++ init.el
@@ -1,11 +1,11 @@
-
 (require 'package)
-(package-initialize)
 
-  (setq package-archives '(("ELPA" . "http://tromey.com/elpa/")
+
+(setq package-archives '(("ELPA" . "http://tromey.com/elpa/")
                          ("marmalade" . "http://marmalade-repo.org/packages/")
                          ("gnu" . "http://elpa.gnu.org/packages/")
                          ("melpa" . "http://melpa.milkbox.net/packages/") ))
+(package-initialize)
 (require 'use-package)
 
 (use-package ace-jump-mode
@@ -14,6 +14,7 @@
   :init
   (bind-key "C-." 'ace-jump-mode))
  
+(bind-key "C-x f" 'find-file)
 (bind-key "C-v" 'clipboard-yank)
 (setq x-select-enable-clipboard t)
 (use-package flatland-theme
